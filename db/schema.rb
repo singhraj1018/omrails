3b42e8e52902973f59e4a9631d5a257a269c2eeb
Rajdeep Singh <singhraj1018@gmail.com>
2017-11-05 23:40:37 -0800

    Before Setting Profiles

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171105192914) do
+ActiveRecord::Schema.define(version: 20171106071350) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -61,8 +61,11 @@ ActiveRecord::Schema.define(version: 20171105192914) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "admin", default: false
+    t.string "username"
+    t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["username"], name: "index_users_on_username", unique: true
   end
 
   add_foreign_key "items", "users"
